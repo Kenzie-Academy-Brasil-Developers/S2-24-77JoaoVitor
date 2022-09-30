f0fe6c9ecf076919f6703572a168af1f19da69d0
77joaovitor <henrique.joaovitor@gmail.com>
2022-09-30 15:28:36 -0300

    att

--- crud.sql
+++ crud.sql
@@ -2,27 +2,60 @@
 -- Simulações de um CRUD
 
 -- Criação
+INSERT INTO pedidos
+	(status, cliente_id)
+	VALUES 
+	('Recebido', 6);
 
--- 1)
+SELECT
+	*
+	FROM 
+	produtos;
+SELECT
+	*
+	FROM 
+	pedidos;
+	
 
-
-
--- 2)
-
-
-
--- 3)
-
-
+		
+	
 
 -- Leitura
-
+SELECT
+	*
+	FROM 
+	produtos;
+SELECT
+	*
+	FROM 
+	pedidos;
+	
 -- 1)
 
+INSERT INTO produtos_pedidos
+(pedido_id, produto_id)
+	VALUES 
+	(11, 1),
+	(11, 2),
+	(11, 6),
+	(11, 8),
+	(11, 8);
 
+    SELECT 
+	*
+	FROM 
+	clientes cl
+	JOIN
+	pedidos pe ON cl.id = pe.cliente_id
+	JOIN 
+	produtos_pedidos pp ON pp.pedido_id = pe.id
+ 	JOIN
+ 	produtos pr ON pr.id = pp.produto_id
+ 	WHERE 
+ 	cl.nome = 'Georgia';
 
 -- Atualização
-
+		
 -- 1)
 
 
--- selects.sql
+++ selects.sql
@@ -1,12 +1,91 @@
 -- Aqui você deve colocar os códigos SQL referentes às
 -- Seleções de dados
+SELECT
+	*
+FROM 
+	pedidos pe
+	JOIN 
+	produtos pr ON pr.id = pe.id;
 
--- 1)
 
--- 2)
+SELECT 
+    pe.id
+FROM 
+	pedidos pe
+JOIN 
+	produtos_pedidos pp ON pe.id = pp.pedido_id
+JOIN 
+	produtos pr ON pr.id = pp.produto_id
+WHERE
+	pr.nome = 'Fritas';
 
--- 3)
+    
+SELECT 
+    cl.nome gostam_de_fritas
+FROM 
+	clientes cl
+JOIN
+	pedidos pe ON cl.id = cliente_id
+JOIN 
+	produtos_pedidos pp ON pe.id = pp.pedido_id
+JOIN 
+	produtos pr ON pr.id = pp.produto_id
+WHERE
+	pr.nome = 'Fritas';
 
--- 4)
 
--- 5)
+SELECT 
+	sum(pr.preço) AS sum
+FROM 
+ 	produtos pr
+JOIN 
+	produtos_pedidos pp ON pr.id =  pp.produto_id
+JOIN 
+	pedidos pe ON pe.id = pp.pedido_id
+JOIN 
+	clientes cl ON cl.id = pe.cliente_id
+WHERE 
+	cl.nome ILIKE  '%Laura%';
+
+INSERT INTO clientes 
+	(nome,lealdade)
+	VALUES
+	('Georgia', 0);
+
+INSERT INTO pedidos
+	(status, cliente_id)
+	VALUES 
+	('Recebido', 6);
+
+SELECT
+	*
+	FROM 
+	produtos;
+SELECT
+	*
+	FROM 
+	pedidos;
+	
+INSERT INTO produtos_pedidos
+(pedido_id, produto_id)
+	VALUES 
+	(11, 1),
+	(11, 2),
+	(11, 6),
+	(11, 8),
+	(11, 8);
+
+SELECT 
+	*
+	FROM 
+	clientes cl
+	JOIN
+	pedidos pe ON cl.id = pe.cliente_id
+	JOIN 
+	produtos_pedidos pp ON pp.pedido_id = pe.id
+ 	JOIN
+ 	produtos pr ON pr.id = pp.produto_id
+ 	WHERE 
+ 	cl.nome = 'Georgia';
+		
+	
